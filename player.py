dc6e08c61358ca5bf929790d06782ac86cd1b2c4
Max Veasey <mveasey87@gmail.com>
2018-02-10 13:44:36 +0000

    added __repr__ and __name__ special methods to Player

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 """Player Class."""
 
+
 class Player():
     """Player class"""
     def __init__(self, name):
@@ -9,3 +10,9 @@ class Player():
         self.word = ''
         self.guess = ''
         self.misses = []
+
+    def __repr__(self):
+        return f'{self.__class__.__name__!r}({self.name!r})'
+
+    def __str__(self):
+        return f'Player: Name = {self.name}'
